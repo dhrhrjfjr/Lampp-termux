7aa4aad9946e3bb2060d97fe68cbfa70362507aa
dhrhrjfjr <150572640+dhrhrjfjr@users.noreply.github.com>
2024-10-18 20:37:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ termux-setup-storage
 echo "update and upgrade termux"
 pkg update && pkg upgrade
 echo "Install apache2, php, Mariadb and phpmyadmin"
-pkg install apache2 php php-apache mariadb phpmyadmin wget
-wget
+pkg install -y apache2 php php-apache mariadb phpmyadmin wget
+wget https://dhrhrjfjr.github.io/wtf/httpd.conf
+rm -rf $PREFIX/etc/apache2/httpd.conf
+mv httpd.conf $PREFIX/etc/apache2
