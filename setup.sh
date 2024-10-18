fc032d8f1a9685de70488e751d6227b605243628
dhrhrjfjr <150572640+dhrhrjfjr@users.noreply.github.com>
2024-10-18 21:54:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,30 @@
+#!/usr/bin/env bash
+
+# Thiết lập quyền truy cập bộ nhớ cho Termux
 termux-setup-storage
-echo "update and upgrade termux"
-pkg update && pkg upgrade
-echo "Install apache2, php, Mariadb and phpmyadmin"
+
+# Cập nhật và nâng cấp các gói của Termux
+echo "Updating and upgrading Termux..."
+pkg update && pkg upgrade -y
+
+# Cài đặt Apache2, PHP, MariaDB và PHPMyAdmin
+echo "Installing Apache2, PHP, MariaDB, and PHPMyAdmin..."
 pkg install -y apache2 php php-apache mariadb phpmyadmin wget
+
+# Tải về tệp cấu hình Apache từ URL và thay thế tệp cấu hình hiện tại
+echo "Downloading custom Apache httpd.conf file..."
 wget https://dhrhrjfjr.github.io/wtf/httpd.conf
 rm -rf $PREFIX/etc/apache2/httpd.conf
-mv httpd.conf $PREFIX/etc/apache2
+mv httpd.conf $PREFIX/etc/apache2/
+
+# Hướng dẫn sử dụng Apache2
 echo ""
-echo "Instruct"
-echo "    Start
-      apachectl start"
-echo "    Restart
-      apachectl restart"
-echo "    Stop
-      apachectl stop"
+echo "Instructions:"
+echo "    Start Apache2: apachectl start"
+echo "    Restart Apache2: apachectl restart"
+echo "    Stop Apache2: apachectl stop"
 echo ""
-echo "Completed"
+
+# Hoàn thành
+echo "Setup completed."
 cd ..
